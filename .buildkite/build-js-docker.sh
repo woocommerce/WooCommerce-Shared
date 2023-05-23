6e29b3db529f56d9b977548740f9d3d7ca2c610b
Gio Lodi <gio@mokacoding.com>
2023-05-23 22:35:33 +1000

    Remove AWS setup logic in JS bundles CI script

--- .buildkite/build-js-docker.sh
+++ .buildkite/build-js-docker.sh
@@ -1,16 +1,5 @@
 #!/bin/bash -eu
 
-echo '--- :s3: Setup environment for S3'
-# Get a temporary session token which will be passed to the Docker container
-sts_token=$(aws sts get-session-token)
-key_id=$(echo "$sts_token" | jq -r .Credentials.AccessKeyId)
-access_key=$(echo "$sts_token" | jq -r .Credentials.SecretAccessKey)
-session_token=$(echo "$sts_token" | jq -r .Credentials.SessionToken)
-
-export AWS_ACCESS_KEY_ID="$key_id"
-export AWS_SECRET_ACCESS_KEY="$access_key"
-export AWS_SESSION_TOKEN="$session_token"
-
 echo '--- :yarn: Restore yarn cache, if any'
 ARCHITECTURE=$(uname -m)
 JS_VERSION=$(cat .nvmrc)

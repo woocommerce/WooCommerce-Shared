1aec369f7115a1112a377bcc0c6618212d6b5daa
Oguz Kocer <oguz.kocer@automattic.com>
2023-06-05 16:20:55 -0400

    Copy Android JS bundle to Android project assets before publishing it

--- .buildkite/publish-android.sh
+++ .buildkite/publish-android.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
 cd ./libraries/android/
-./gradlew -PwillPublishBinary=true :library-native-bridge:prepareToPublishToS3 `prepare_to_publish_to_s3_params` :library:publish
+mkdir -p library/build/assets
+buildkite-agent artifact download dist/bundles/bundle-android.js library/build/assets/index.android.bundle
+
+./gradlew -PwillPublishBinary=true :library:prepareToPublishToS3 `prepare_to_publish_to_s3_params` :library:publish
 

e7da178cdb808e0bbe91574feae07588c7215f3d
Oguz Kocer <oguz.kocer@automattic.com>
2023-06-05 17:00:33 -0400

    Fix buildkite bundled android js artifact path

--- .buildkite/publish-android.sh
+++ .buildkite/publish-android.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
-cd ./libraries/android/
-mkdir -p library/build/assets
-buildkite-agent artifact download dist/bundles/bundle-android.js library/build/assets/index.android.bundle
+# Copy the JavaScript bundle
+buildkite-agent artifact download dist/bundles/bundle-android.js .
 
+mkdir -p ./libraries/android/library/build/assets
+cp ./dist/bundles/bundle-android.js ./libraries/android/library/build/assets/index.android.bundle
+
+cd ./libraries/android/library
 ./gradlew -PwillPublishBinary=true :library:prepareToPublishToS3 `prepare_to_publish_to_s3_params` :library:publish
 

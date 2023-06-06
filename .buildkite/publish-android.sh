6886fc67d845235d28ca2bc492faddf0098ffbf8
Oguz Kocer <oguz.kocer@automattic.com>
2023-06-06 12:21:26 -0400

    Create gradle.properties file before publishing Android artifacts

--- .buildkite/publish-android.sh
+++ .buildkite/publish-android.sh
@@ -7,5 +7,6 @@ mkdir -p ./libraries/android/library/build/assets
 cp ./dist/bundles/bundle-android.js ./libraries/android/library/build/assets/index.android.bundle
 
 cd ./libraries/android/
+cp gradle.properties-example gradle.properties
 ./gradlew -PwillPublishBinary=true :library:prepareToPublishToS3 `prepare_to_publish_to_s3_params` :library:publish
 

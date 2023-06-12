6a31abb6ace838632516cbe282eb99422cddc32e
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2023-06-12 16:50:19 -0600

    Fix mutability issue

--- libraries/ios/Demo App/WCReactNativeViewController+LocalDevelopment.swift
+++ libraries/ios/Demo App/WCReactNativeViewController+LocalDevelopment.swift
@@ -2,7 +2,7 @@ import WooCommerceShared
 
 extension WCReactNativeViewController {
     static func forLocalDevelopment(onPort port: UInt16 = 8081) -> WCReactNativeViewController {
-        var url = URL(string: "http://localhost:\(port)/index.bundle?platform=ios")!
+        let url = URL(string: "http://localhost:\(port)/index.bundle?platform=ios")!
         return WCReactNativeViewController(bundle: url)
     }
 }

1b92419ae7816670e181efa19711d241ea28b5cd
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2023-06-12 17:10:27 -0600

    Add on-device development too

--- libraries/ios/Demo App/WCReactNativeViewController+LocalDevelopment.swift
+++
@@ -1,8 +0,0 @@
-import WooCommerceShared
-
-extension WCReactNativeViewController {
-    static func forLocalDevelopment(onPort port: UInt16 = 8081) -> WCReactNativeViewController {
-        let url = URL(string: "http://localhost:\(port)/index.bundle?platform=ios")!
-        return WCReactNativeViewController(bundle: url)
-    }
-}

542d9d7c45411ba31a9e79b61ba75c1deb22d26b
Ernesto Carrión <ernesto.carrion@automattic.com>
2023-06-28 03:04:59 -0500

    Update WooCommerceShared to receive an external AnalyticsProvider

--- libraries/ios/WooCommerceShared/WCReactNativeViewController.h
+++ libraries/ios/WooCommerceShared/WCReactNativeViewController.h
@@ -1,10 +1,14 @@
 #import <UIKit/UIKit.h>
+#import "WCRNAnalyticsProvider.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface WCReactNativeViewController : UIViewController
 -(instancetype)init;
--(instancetype)initWithBundle:(NSURL *) url;
+-(instancetype)initWithBundle:(NSURL *) url
+            analyticsProvider:(id<WCRNAnalyticsProvider>) analyticsProvider
+                       blogID:(NSString *)blogId
+                     apiToken: (NSString*) apiToken;
 @end
 
 NS_ASSUME_NONNULL_END

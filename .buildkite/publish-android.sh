ba05f05035d671e310bcfa377ce8ce7d50fcf7bd
Oguz Kocer <oguz.kocer@automattic.com>
2023-07-03 20:01:37 -0400

    Publish React Native Android libraries from Buildkite

--- .buildkite/publish-android.sh
+++ .buildkite/publish-android.sh
@@ -24,7 +24,23 @@ cp ./dist/bundles/bundle-android.js ./libraries/android/library/build/assets/ind
 
 cd ./libraries/android/
 
+echo "--- :android: Publishing React Native Libraries"
+
+REACT_NATIVE_PROJECTS_TO_PUBLISH=(
+react-native-async-storage_async-storage
+react-native-safe-area-context
+react-native-screen
+)
+
+for project in "${PROJECTS[@]}"
+do
+    ./gradlew :$project:prepareToPublishToS3 `prepare_to_publish_to_s3_params` :$project:publish
+done
+
+echo "--- :android: Publishing WooCommerce Shared Library"
+
+VERSION_TO_PUBLISH=`./gradlew -q :library:calculateVersionName `prepare_to_publish_to_s3_params``
 # :demo:preBuild is necessary because React Native Gradle Plugin only works if a `com.android.application`
 # module is part of the build. We don't need to build the demo app, so the `preBuild` task is enough for us
-./gradlew -PwillPublishWooCommerceSharedLibrary=true :demo:preBuild :library:prepareToPublishToS3 `prepare_to_publish_to_s3_params` :library:publish
+./gradlew -PwillPublishWooCommerceSharedLibrary=true -PreactNativeLibrariesPublishedVersion=VERSION_TO_PUBLISH :demo:preBuild :library:prepareToPublishToS3 `prepare_to_publish_to_s3_params` :library:publish
 

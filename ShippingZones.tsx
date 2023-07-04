bc240a8f4a97708f94bb1154adb3380223015911
Ernesto Carrión <ernesto.carrion@automattic.com>
2023-07-04 16:59:10 -0500

    Adjust back button to iOS settings too

--- ShippingZones.tsx
+++ ShippingZones.tsx
@@ -13,13 +13,12 @@ import { fetchShippingZones, ShippingZone } from "./API/ShippingZoneAPI";
 import { useNavigation } from "@react-navigation/native";
 import { NavigationRoutes } from "./Navigation/NavigationRoutes";
 import ToolbarActionButton from "./ToolbarActionButton";
-import { NativeModules } from 'react-native';
-import {HeaderBackButton} from "@react-navigation/elements";
+import { NativeModules } from "react-native";
+import { HeaderBackButton } from "@react-navigation/elements";
 
 const sendAnalyticsEvent = (event) => {
   NativeModules.AnalyticsModule.sendEvent(event);
 };
-
 
 type RowProps = {
   title: string;
@@ -78,7 +77,7 @@ const ShippingZonesList = () => {
     try {
       const zones = await fetchShippingZones();
       setData(zones);
-      sendAnalyticsEvent('shipping_zones_shown');
+      sendAnalyticsEvent("shipping_zones_shown");
     } catch (error) {
       console.log(error);
       showRetryAlert();
@@ -95,13 +94,17 @@ const ShippingZonesList = () => {
 
   useEffect(() => {
     navigation.setOptions({
-      headerLeft: () => <HeaderBackButton
-          style={{marginLeft: Platform.OS === "ios" ? -15 : -5}}
-          labelVisible={Platform.OS === "ios"}
+      headerLeft: () => (
+        <HeaderBackButton
+          tintColor={Platform.OS === "ios" ? "rgb(103, 67, 153)" : undefined}
+          style={{ marginLeft: Platform.OS === "ios" ? -15 : -5 }}
           label="Settings"
+          labelVisible={false}
           onPress={() => {
             NativeModules.ExitModule.exit();
-          }}/>,
+          }}
+        />
+      ),
       headerRight: () => (
         <ToolbarActionButton
           label={"Add"}
